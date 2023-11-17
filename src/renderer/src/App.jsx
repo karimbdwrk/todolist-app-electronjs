75fec7bc15202d0453698c7c63dce6d75ffa93ab
Karim Badwork <karim@badwork.fr>
2023-11-17 11:30:38 +0100

    redux

---
+++ src/renderer/src/App.jsx
@@ -0,0 +1,12 @@
+import Versions from './components/Versions'
+import icons from './assets/icons.svg'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
